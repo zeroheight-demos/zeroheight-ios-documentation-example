cd63b768c818eacc616c467875e1f7b82526097e
Seth Corker <seth@zeroheight.com>
2025-07-01 11:38:32 +0100

    Initial Commit

---
+++ zeroheight Documentation Example/zeroheight_Documentation_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  zeroheight_Documentation_ExampleApp.swift
+//  zeroheight Documentation Example
+//
+//  Created by Seth Corker on 01/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct zeroheight_Documentation_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
